7adedb27c34dadf2ae8554dbfeabb2c5b02e8992
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-07 17:23:33 -0300

    Update AnimeServiceApplication.java

--- services/anime-service/src/main/java/br/com/devdojo/spring_boot/anime_service/AnimeServiceApplication.java
+++ services/anime-service/src/main/java/br/com/devdojo/spring_boot/anime_service/AnimeServiceApplication.java
@@ -8,6 +8,7 @@ public class AnimeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnimeServiceApplication.class, args);
+        System.out.println("Aplicação iniciada");
 	}
 
 }
