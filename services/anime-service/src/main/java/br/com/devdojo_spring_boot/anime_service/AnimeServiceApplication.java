5d5182ac54a82728eb2d1bd866a2e002f8eac874
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-07 15:23:27 -0300

    Initial commit

---
+++ services/anime-service/src/main/java/br/com/devdojo_spring_boot/anime_service/AnimeServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.devdojo_spring_boot.anime_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeServiceApplication.class, args);
+	}
+
+}
---
+++ services/anime-service/src/test/java/br/com/devdojo_spring_boot/anime_service/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devdojo_spring_boot.anime_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
