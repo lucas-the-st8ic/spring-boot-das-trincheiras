0c631199f30c5de5ee3ddb73ed774a4edf787a42
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-08 16:07:43 -0300

    updates

--- services/anime-service/src/main/java/br/com/devdojo/spring_boot/AnimeServiceApplication.java
+++ services/anime-service/src/main/java/br/com/devdojo/spring_boot/AnimeServiceApplication.java
@@ -3,10 +3,12 @@ package br.com.devdojo.spring_boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"outside.devdojo", "br.com.devdojo.spring_boot"})
 public class AnimeServiceApplication {
 
 	public static void main(String[] args) {
