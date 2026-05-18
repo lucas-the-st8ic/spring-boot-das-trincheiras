73d8c7cba831e7a3722a008a123b8e2cf0694928
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-18 12:04:16 -0300

    Update AnimeController.java

--- services/anime-service/src/main/java/br/com/devdojo/spring_boot/controller/AnimeController.java
+++ services/anime-service/src/main/java/br/com/devdojo/spring_boot/controller/AnimeController.java
@@ -2,10 +2,7 @@ package br.com.devdojo.spring_boot.controller;
 
 import br.com.devdojo.spring_boot.domain.Anime;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -18,6 +15,25 @@ public class AnimeController {
     @GetMapping
     public List<Anime> listAll(@RequestParam(required = false)
                                     String name) {
-        return Anime.getAnimes();
+        var animes = Anime.getAnimes();
+
+        if(name == null) {
+            return animes;
+        }
+
+        return animes.stream().filter(anime -> anime.getName()
+                .equalsIgnoreCase(name)).toList();
     }
+
+    @GetMapping("{id")
+    public Anime findById(@PathVariable Long id) {
+
+       return Anime.getAnimes()
+                .stream()
+                .filter(anime -> anime.getId()
+                        .equals(id))
+                .findFirst().orElse(null);
+
+    }
+
 }
