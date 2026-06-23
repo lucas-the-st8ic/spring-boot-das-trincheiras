4e6337887d98abdc0c41e2c2c284ff5651e863bc
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-06-23 12:46:01 -0300

    Updates

--- services/anime-service/src/main/java/br/com/devdojo/spring_boot/repository/ProducerHardCodedRepository.java
+++ services/anime-service/src/main/java/br/com/devdojo/spring_boot/repository/ProducerHardCodedRepository.java
@@ -23,7 +23,7 @@ public class ProducerHardCodedRepository {
         PRODUCERS.addAll(List.of(madHouse, studioBones, toeiAnimation));
     }
 
-    public static List<Producer> findAll() {
+    public  List<Producer> findAll() {
         return PRODUCERS;
     }
 
