0aae51b22fe144d014fd9f060fbecaf33afb8d5f
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-03 11:40:58 -0300

    Update ProducerMapper.java

--- services/anime-service/src/main/java/br/com/devdojo/spring_boot/mapper/ProducerMapper.java
+++ services/anime-service/src/main/java/br/com/devdojo/spring_boot/mapper/ProducerMapper.java
@@ -8,13 +8,12 @@ import br.com.devdojo.spring_boot.response.ProducerPostResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ProducerMapper {
-    
+
     @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     @Mapping(target = "id", expression = "java(java.util.concurrent.ThreadLocalRandom.current().nextLong(100_000))")
     Producer toProducer(ProducerPostRequest postRequest);
